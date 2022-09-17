ad0c3f98e0c4c6783528bc9b178777afdd69e784
vlad <vlad@root>
2022-09-17 14:37:27 +0300

    update

---
+++ src/components/UI/input/inputFile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import classes from './inputfile.module.css'
+
+const InputFile = ({onChange, name, value = null}) => {
+    return (
+        <div style={{display: 'flex', whiteSpace: "nowrap", verticalAlign: 'middle'}}>
+            <input
+                id={name}
+                type='file'
+                name={name}
+                onChange={onChange}
+                accept='.pdf,.docx,.doc'
+                className={classes.fileInput}
+                style={{display: "none"}}
+            />
+            <label id='nameFile' htmlFor={name}>Файл</label>
+            {value !== null
+                ? <label id='nameFile' style={{margin: 'auto'}}>{value.name}</label>
+
+                : <label id='nameFile' style={{margin: 'auto'}}>Файл не вибраний</label>
+
+            }
+        </div>
+
+    );
+};
+
+export default InputFile;
